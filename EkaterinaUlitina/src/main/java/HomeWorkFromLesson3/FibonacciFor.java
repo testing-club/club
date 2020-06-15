453eb52b014e54b1873537c1244d044f3b858cb7
UlitinaKate <ulitina.kate@gmail.com>
2020-06-15 14:48:54 +0200

    Add three files to branch B and rename one of previous files

---
+++ EkaterinaUlitina/src/main/java/HomeWorkFromLesson3/FibonacciFor.java
@@ -0,0 +1,7 @@
+package HomeWorkFromLesson3;
+
+public class FibonacciFor {
+    public static void main(String[] args) {
+        System.out.println("Will write it later");
+    }
+}
--- EkaterinaUlitina/src/main/java/HomeWorkFromLesson3/FibonacciWhile.java
+++ EkaterinaUlitina/src/main/java/HomeWorkFromLesson3/FibonacciWhile.java
@@ -1,6 +1,6 @@
 package HomeWorkFromLesson3;
 
-public class Factorial {
+public class FibonacciWhile {
     public static void main(String[] args) {
         System.out.println("Will write it later");
     }
