cdc206912396fedbf6a0af4cc7bd1ee3ce7dce44
oberdnik <onberdnik@gmail.com>
2020-06-26 00:56:08 +0200

    add lesson 6

---
+++ OlgaBerdnik/src/main/java/Lesson_6/Exampl3.java
@@ -0,0 +1,20 @@
+package Lesson_6;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class lesson {
+    public static void main (String[] args) {
+        //Fill 2-dimentional array with random numbers {{},{}}
+
+        int [][] arr1 = new int[8][15];
+
+        for (int i=0; i<arr1.length; i++){
+            for (int j=0; j<arr1[i].length; j++) {
+                arr1[i][j] = new Random().nextInt(10);
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+      System.out.println(  Arrays.deepToString(arr1)); // вывод всего массива
+}}
---
+++ OlgaBerdnik/src/main/java/Lesson_6/Example4.java
@@ -0,0 +1,4 @@
+package Lesson_6;
+
+public class Example4 {
+}
