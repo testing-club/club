99dc648ea82da4d878c7ecbe06d6d4071c9b9b8d
DGrytsaieva <daryna.grytsaieva@dazn.com>
2020-06-26 10:22:46 +0200

    Daryna - Add lesson 6 examples

---
+++ DarynaGrytsaieva/src/main/java/lesson6/Example4.java
@@ -0,0 +1,25 @@
+package src.main.java.lesson6;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Example4 {
+    public static void main(String[] args) {
+
+        //Fill 3-dimentional array with 0
+
+        int[][][] arr1 = new int[8][15][10];
+
+        for (int i = 0; i <arr1.length ; i++) {
+            for (int j = 0; j < arr1[i].length ; j++) {
+
+
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
