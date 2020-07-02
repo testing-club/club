833821748349b6ca4892a3330445aacb0a4f2f97
UlitinaKate <ulitina.kate@gmail.com>
2020-07-02 16:11:05 +0200

    Homeork 8. Zeros and stars

---
+++ EkaterinaUlitina/src/main/java/HomeWorkFromLesson6/ZerosAndStars.java
@@ -0,0 +1,19 @@
+package HomeWorkFromLesson6;
+
+public class ZerosAndStars {
+    public static void main(String[] args) {
+
+        char[][] arr = new char[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == j || j == 9 - i) {
+                    arr[i][j] = '*';
+                } else {
+                    arr[i][j] = '0';
+                }
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
