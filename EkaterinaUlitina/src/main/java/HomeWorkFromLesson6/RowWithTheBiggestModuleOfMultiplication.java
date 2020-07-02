0b6ec0cfa1c6499e7273770ac932b16e76fda0e0
UlitinaKate <ulitina.kate@gmail.com>
2020-07-02 13:12:08 +0200

    Homework 5. Max module multiplication

---
+++ EkaterinaUlitina/src/main/java/HomeWorkFromLesson6/RowWithTheBiggestModuleOfMultiplication.java
@@ -0,0 +1,34 @@
+package HomeWorkFromLesson6;
+
+import java.util.Random;
+
+public class RowWithTheBiggestModuleOfMultiplication {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[7][4];
+        Random random = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = random.nextInt(11) - 5;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int rowIndex = 0;
+        int module = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int multiplication = 1;
+            for (int j = 0; j < arr[0].length; j++) {
+                multiplication *= arr[i][j];
+                if (Math.abs(multiplication) > module) {
+                    module = Math.abs(multiplication);
+                    rowIndex = i;
+                }
+            }
+        }
+        System.out.println("Index of row with the max multiplication: " + rowIndex);
+
+    }
+}
