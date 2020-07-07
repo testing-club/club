4ecc0127ac0a4a05cb41419daa5e7f8922e238eb
Sheraswishes <sheraswishes@gmail.com>
2020-07-07 22:31:00 +0200

    Corrected Vera_Lesson6_HW_Ex2

--- VeraSH/src/main/java/Vera_Lesson6_HW_Ex2.java
+++ VeraSH/src/main/java/Vera_Lesson6_HW_Ex2.java
@@ -12,39 +12,34 @@ public class Vera_Lesson6_HW_Ex2 {
         for (int i = 0; i < arr1.length; i++) { // interaction over elements
             arr1[i] = new Random().nextInt(100);
 
-            System.out.print(arr1[i] + " ");
+            System.out.print(arr1[i] + "\t");
         }
         System.out.print("\n");
         selectionSort(arr1);
-
     }
-    public static void selectionSort (int[] arr1){
+
+    public static void selectionSort(int[] arr1) {
     /*По очереди будем просматривать все подмножества
       элементов массива (0 - последний, 1-последний,
       2-последний,...)*/
-            for (int i = 0; i < arr1.length; i++) {
+        for (int i = 0; i < arr1.length; i++) {
         /*Предполагаем, что первый элемент (в каждом
            подмножестве элементов) является минимальным */
-                int min = arr1[i];
-                int min_i = i;
+            int min = arr1[i];
+            int minNumber = i;
         /*В оставшейся части подмножества ищем элемент,
            который меньше предположенного минимума*/
-                for (int j = i + 1; j < arr1.length; j++) {
-                    //Если находим, запоминаем его индекс
-                    if (arr1[j] < min) {
-                        min = arr1[j];
-                        min_i = j;
-                    }
+            for (int j = i + 1; j < arr1.length; j++) {
+                //Если находим, запоминаем его индекс
+                if (arr1[j] < min) {
+                    min = arr1[j];
+                    minNumber = j;
                 }
-        /*Если нашелся элемент, меньший, чем на текущей позиции,
-          меняем их местами*/
-                if (i != min_i) {
-                    int tmp = arr1[i];
-                    arr1[i] = arr1[min_i];
-                    arr1[min_i] = tmp;
-                }
-                System.out.print(arr1[i] + " ");
             }
-
+            arr1[minNumber] = arr1[i];
+            arr1[i] = min;
+            System.out.print(arr1[i] + "\t");
         }
+
     }
+}
