bd8bb9fa580e766b81e41465ecdc1408c1b1bcbf
Rita Kovaleva <margaritasinyuk@gmail.com>
2020-07-08 19:45:31 +0200

    Add lesson 8

---
+++ RitaKovaleva/src/main/java/lesson_8/Example1.java
@@ -0,0 +1,22 @@
+package lesson_8;
+
+public class Example1 {
+    public static void main(String[] args) {
+        // Inheritance example
+        Computer comp1 = new Computer("Intel", 2000,15);
+        comp1.printInfo();
+
+        Laptop lamp1 = new Laptop("Lenovo",3000,2,8);
+        lamp1.printInfo();
+
+        Tablet tab1 = new Tablet("Apple", 1000, 1, 5, "Resistive");
+        tab1.printInfo();
+
+        // Abstract class example
+
+        // Doctor doc = new Doctor(); -error
+
+        Surgeon surg = new Surgeon("Tom", "Summer", "ER", 3500);
+        surg.printData();
+    }
+}
---
+++ RitaKovaleva/src/main/java/lesson_8/Tablet.java
@@ -0,0 +1,24 @@
+package lesson_8;
+
+public class Tablet extends Laptop {
+    private String touchScreen;
+
+    public Tablet(String brand, double price, double weight, int batteryHours, String touchScreen) {
+        super(brand, price, weight, batteryHours);
+        this.touchScreen = touchScreen;
+    }
+
+    public String getTouchScreen() {
+        return touchScreen;
+    }
+
+    public void setTouchScreen(String touchScreen) {
+        this.touchScreen = touchScreen;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.printf("Touchscreen is %s.", touchScreen);
+    }
+}
