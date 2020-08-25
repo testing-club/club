2e179bda0422560d0163a5ee9bf2585a9b478a40
Rita Kovaleva <margaritasinyuk@gmail.com>
2020-08-25 16:22:32 +0200

    Add solution with issie for test assignment

--- RitaKovaleva/src/main/java/lesson9/example/Animal.java
+++ RitaKovaleva/src/main/java/lesson9/example/Animal.java
@@ -3,6 +3,6 @@ package lesson9.example;
 public class Animal implements Swimmable {
     @Override
     public void swim() {
-        System.out.println("I am a duck. I am swimming.");
+        System.out.println("I am a duck. I like swimming.");
     }
 }
